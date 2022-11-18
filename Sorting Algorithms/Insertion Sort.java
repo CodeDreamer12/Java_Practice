952522f5539aeb879600eae6ba3f6983902b9612
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-11-18 22:38:33 +0530

    Create Insertion Sort.java

---
+++ Sorting Algorithms/Insertion Sort.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args){
+        int arr[]={8,4,9,1,10,6};
+        for(int i=1; i<arr.length; i++){
+            int curr=arr[i];
+            int j=i-1;
+            while(j>=0 && curr<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=curr;
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
