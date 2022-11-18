03a6a0e03c1d13de023ea024d45475522ca89c56
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-11-18 22:37:42 +0530

    Create Bubble Sort.java

---
+++ Sorting Algorithms/Bubble Sort.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) {
+	   int arr[]={7,9,8,2,4,5};
+	   for(int i=0; i<arr.length-1; i++){
+	       for(int j=0; j<arr.length-i-1; j++){
+	           if(arr[j]>arr[j+1]){
+	               int temp=arr[j];
+	               arr[j]=arr[j+1];
+	               arr[j+1]=temp;
+	           }
+	       }
+	   }
+	   for(int i=0; i<arr.length; i++){
+	       System.out.print(arr[i]+" ");
+	   }
+	   
+	}
+}
